3fd1c329254852ef52cbed323036f9306c5fee19
lavaspoon <lavaspoon@naver.com>
2021-11-24 21:47:48 +0900

    //주석처리

--- module-test/src/main/java/playground/moduletest/AppConfig.java
+++ module-test/src/main/java/playground/moduletest/AppConfig.java
@@ -11,21 +11,36 @@ import playground.moduletest.order.OrderService;
 import playground.moduletest.order.OrderServiceImpl;
 
 public class AppConfig {
-    public MemberService memberService() {
-        //생성자 주입
-        return new MemberServiceImpl(getMemberRepository()); //MemoryMemberRepository를 넘겨주면 생성자에 주입됨
+//    public MemberService memberService() {
+//        //생성자 주입
+//        return new MemberServiceImpl(new MemoryMemberRepository()); //MemoryMemberRepository를 넘겨주면 생성자에 주입됨
+//    }
+//
+//    public OrderService orderService() {
+//        //생성자 주입 (DiscountPolicy - FixDiscount, RateDiscount 선택)
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
+    /**
+     * 리팩토링
+     */
+        public MemberService memberService() {
+            //생성자 주입
+            return new MemberServiceImpl(getMemberRepository()); //MemoryMemberRepository를 넘겨주면 생성자에 주입됨
+        }
+
+        private MemberRepository getMemberRepository() {
+            return new MemoryMemberRepository();
+        }
+
+        public OrderService orderService() {
+            //생성자 주입 (DiscountPolicy - FixDiscount, RateDiscount 선택)
+            return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+        }
+
+        private DiscountPolicy discountPolicy() {
+            return new RateDiscountPolicy();
+        }
     }
 
-    private MemberRepository getMemberRepository() {
-        return new MemoryMemberRepository();
-    }
-
-    public OrderService orderService() {
-        //생성자 주입 (DiscountPolicy - FixDiscount, RateDiscount 선택)
-        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
-    }
-
-    private DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
-    }
 }
