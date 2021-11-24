7e71772dd458e16f4798d486aebb94e211fbbf10
lavaspoon <lavaspoon@naver.com>
2021-11-24 21:17:53 +0900

    AppConfig 리펙토링

--- module-test/src/main/java/playground/moduletest/AppConfig.java
+++ module-test/src/main/java/playground/moduletest/AppConfig.java
@@ -1,7 +1,9 @@
 package playground.moduletest;
 
+import playground.moduletest.discount.DiscountPolicy;
 import playground.moduletest.discount.FixDiscountPolicy;
 import playground.moduletest.discount.RateDiscountPolicy;
+import playground.moduletest.model.member.MemberRepository;
 import playground.moduletest.model.member.MemberService;
 import playground.moduletest.model.member.MemberServiceImpl;
 import playground.moduletest.model.member.MemoryMemberRepository;
@@ -11,11 +13,19 @@ import playground.moduletest.order.OrderServiceImpl;
 public class AppConfig {
     public MemberService memberService() {
         //생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository()); //MemoryMemberRepository를 넘겨주면 생성자에 주입됨
+        return new MemberServiceImpl(getMemberRepository()); //MemoryMemberRepository를 넘겨주면 생성자에 주입됨
+    }
+
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         //생성자 주입 (DiscountPolicy - FixDiscount, RateDiscount 선택)
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
