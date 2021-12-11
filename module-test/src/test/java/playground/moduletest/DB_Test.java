23991efa1729fa89d92dcc76160f34393ec62b51
lavaspoon <lavaspoon@naver.com>
2021-12-12 01:33:39 +0900

    오라클 연동 테스트 성공

--- module-test/src/test/java/playground/moduletest/DB_Test.java
+++ module-test/src/test/java/playground/moduletest/DB_Test.java
@@ -1,15 +1,33 @@
 package playground.moduletest;
 
-import org.junit.jupiter.api.Test;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DB_Test {
-    @Test
-    public void test() throws Exception {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/user_db","root","qkqh12");
-        System.out.println(con);
+    //로깅을 위한 변수 선언
+    private static final Logger logger=
+            LoggerFactory.getLogger(DB_Test.class);
+    private static final String DRIVER
+            ="oracle.jdbc.driver.OracleDriver";
+    //연결문자열 jdbc:oracle:thin:@호스트:포트:sid
+    private static final String URL
+            ="jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USER="system";//아이디
+    private static final String PW="oracle";//
+
+    @Test//Junit이 테스트하는 메소드
+    public void test() throws ClassNotFoundException {
+        Class.forName(DRIVER);//jdbc 드라이버 로딩
+
+        //괄호안에 리소스 자동 삭제
+        try(Connection conn=DriverManager.getConnection(URL, USER, PW)){
+            logger.info("success");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
